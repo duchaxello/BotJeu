98bc758315ebe402cc3efac663c0aac216461b28
Louis Du Chaxel <louis.du.chaxel@eisti.fr>
2018-11-05 15:44:44 +0100

    On peut maintenant admirer le personnage de n'importe qui

--- commands/admirerLePuissant.js
+++ commands/admirerLePuissant.js
@@ -1,16 +1,28 @@
 const database = require('../functions/db.js');
 const embed = require('../functions/embed.js');
+const { getUserFromMention } = require('../functions/mentions.js');
 
 module.exports = {
   name: 'admire',
-  description: 'Montre les stats de Maglubiyet Le Puissant',
+  description: 'Montre les stats de Maglubiyet Le Puissant ou d\'un gobelin mentionné',
   args: false,
   usage: '',
   async execute(message, args, lang) {
-    // database.gods.addMaglubiyet();
-    // const maglubiyet = await (database.gods.findOne({}));
-    // console.log('Maglubiyet ---> ', maglubiyet);
-    database.gods.findOne({})
+    if (args[0]) {
+      const user = getUserFromMention(args[0]);
+      if (!user) {
+        return message.reply(lang.wrongMentionForAdmiration);
+      }
+      database.characters.getCharacterByUserId(user.id)
+        .then(function(response) {
+          if (response == null) {
+            message.channel.send(lang.noCharacterForAdmiration);
+          } else {
+            message.channel.send(embed.character(response));
+          }
+        })
+    } else {
+      database.gods.findOne({})
       .then(function(response) {
         if (response == null) {
           throw 'ERROR : no god available';
@@ -20,5 +32,6 @@ module.exports = {
       .catch((err) => {
         throw err;
       });
+    }
   }
 };
--- textes/textes.js
+++ textes/textes.js
@@ -16,7 +16,8 @@ fr.welcomeOtherMember = function(user) {
 fr.welcomeEveryone = ' Bienvenue à tous dans la grotte de la Crasse Oubliée !';
 
 /* Commande 'admire' */
-fr.wrongMentionForAdmiration = "Ce misérable ne mérite aucune admiration.";
+fr.wrongMentionForAdmiration = 'Ce misérable n\'existe pas.';
+fr.noCharacterForAdmiration = 'Inconnu au bataillon';
 
 /* Textes pour la création de personnages */
 fr.chooseToCreateCharacter = 'Je vois que tu veux te réincarner, bien bien bien.' +
